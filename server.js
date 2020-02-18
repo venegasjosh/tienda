1e72e7841b225d6185ddfa1d78b9f7628fae2fec
venegasjosh <47758513+venegasjosh@users.noreply.github.com>
2020-02-18 01:17:21 -0800

    added lines to deploy

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const orders = require("./routes/api/orders");
 const passport = require("passport");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const path = require("path");
 
 
 // // Connect to Mongo:
@@ -61,7 +62,12 @@ app.use("/api/items", items);
 app.use("/api/orders", orders);
 app.use("/api/carts", carts);
 
-
+//Serve Static Assets in production 
+//set static folder
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 
 app.listen(port, () => console.log(`Server Running On Port: ${port}`));
